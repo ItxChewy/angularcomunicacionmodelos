7f4a60d932ab265f18b32e838787f6769161d8d9
ItxChewy <aarongarcia1906@gmail.com>
2024-11-07 14:19:25 +0100

    menu

---
+++ src/app/models/producto.ts
@@ -0,0 +1,16 @@
+export class Producto {
+    // public nombre: string;
+    // public imagen: string;
+    // public precio: number;
+
+    constructor(
+        public nombre: string,
+        public imagen: string,
+        public precio: number,
+    ) {
+        // name: string, image: string, price: number
+        // this.nombre = name;
+        // this.imagen = image;
+        // this.precio = price;
+    }
+}
